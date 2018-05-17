a0fbba8ddbe4183f3ab664db39980a1b5ca588a1
unknown <tadej.rola@gmail.com>
2018-05-17 10:57:13 +0200

    itemlist for "obvestila in dolžnosti" implemented

--- src/components/Obvestila/ItemList.js
+++ src/components/Obvestila/ItemList.js
@@ -1,63 +1,122 @@
-import React, { Component } from 'react'
-import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-} from 'react-native'
-
-
-const items = [
-  { name: 'bla bla 1' },
-  { name: 'jutri ne bo neta 1' },
-  { name: 'popravi to' },
-]
+import React, { Component } from "react";
+import { View, Text, FlatList, ActivityIndicator } from "react-native";
+import { List, ListItem, SearchBar } from "react-native-elements";
 
 class ItemList extends Component {
-
   static navigationOptions = {
-    title: 'Obvestila'
+    title: 'Moja obvestila in obveznosti'
+  }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+      data: [],
+      page: 1,
+      seed: 1,
+      error: null,
+      refreshing: false
+    };
   }
 
-  renderItem = (item, i) => {
-    return (
-      <TouchableOpacity
-        key={i}
-        style={styles.item}
-        onPress={() => this.props.navigation.navigate('ObvestilaItem', { title: item.name })}
-      >
-        <Text style={styles.itemText}>{item.name}</Text>
-      </TouchableOpacity>)
+  componentDidMount() {
+    this.makeRemoteRequest();
   }
+
+  makeRemoteRequest = () => {
+    const { page, seed } = this.state;
+    const url = `http://our-rent-api.herokuapp.com/api/maintenances`;
+    this.setState({ loading: true });
+
+    fetch(url)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          data: res,
+          error: res.error || null,
+          loading: false,
+          refreshing: false
+        });
+        console.log(this.state.data);
+      })
+      .catch(error => {
+        this.setState({ error, loading: false });
+      });
+  };
+
+  handleRefresh = () => {
+    this.setState(
+      {
+        page: 1,
+        seed: this.state.seed + 1,
+        refreshing: true
+      },
+      () => {
+        this.makeRemoteRequest();
+      }
+    );
+  };
+
+
+  renderSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "86%",
+          backgroundColor: "#CED0CE",
+          marginLeft: "14%"
+        }}
+      />
+    );
+  };
+
+  renderHeader = () => {
+    return <SearchBar placeholder="Vnesite iskalni niz" lightTheme round />;
+  };
+
+  renderFooter = () => {
+    if (!this.state.loading) return null;
+
+    return (
+      <View
+        style={{
+          paddingVertical: 20,
+          borderTopWidth: 1,
+          borderColor: "#CED0CE"
+        }}
+      >
+        <ActivityIndicator animating size="large" />
+      </View>
+    );
+  };
 
   render() {
     return (
-      <View style={styles.container}>
-        {items.map(this.renderItem)}
-      </View>
-    )
+      <List containerStyle={{ borderTopWidth: 0, borderBottomWidth: 0 }}>
+        <FlatList
+          data={this.state.data}
+          renderItem={({ item }) => (
+            <ListItem
+              key={item.id}
+              onPress={() => this.props.navigation.navigate('ObvestilaItem', { title: item.country })}
+              roundAvatar
+              title={`${item.description} ${item.fixingCost}`}
+              subtitle={item.dateReported}
+              // avatar={{ uri: item.picture.thumbnail }}
+              containerStyle={{ borderBottomWidth: 0 }}
+            />
+          )}
+          keyExtractor={item => item.id.toString()}
+          ItemSeparatorComponent={this.renderSeparator}
+          ListHeaderComponent={this.renderHeader}
+          ListFooterComponent={this.renderFooter}
+          onRefresh={this.handleRefresh}
+          refreshing={this.state.refreshing}
+        />
+      </List>
+    );
   }
 }
 
-export default ItemList
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    backgroundColor: '#2980b9',
-    padding: 20,
-  },
-  text: {
-    color: 'white',
-    fontSize: 40,
-    fontWeight: 'bold',
-  },
-  item: {
-    padding: 10,
-  },
-  itemText: {
-    color: 'white',
-    fontSize: 20,
-  }
-})
+export default ItemList;
