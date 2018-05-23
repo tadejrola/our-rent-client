981e6eed0cce872b9ce3b6f7f0cd37e3b8273f03
unknown <tadej.rola@gmail.com>
2018-05-23 21:43:21 +0200

    markers real data fixed!!

--- src/components/MojeNepremicnine/MojeNepremicnineZemljevid.js
+++ src/components/MojeNepremicnine/MojeNepremicnineZemljevid.js
@@ -24,69 +24,44 @@ class MojeNepremicnineZemljevid extends Component {
     }
     componentWillMount() {
 
-        //TEST DATA
-        const placesArray = [];
-        placesArray.push({
-            latitude: 45,
-            longitude: 15,
-            id: "1",
-            title: "test1",
-            description: "test1 desc"
-        });
-        placesArray.push({
-            latitude: 46,
-            longitude: 15,
-            id: "2",
-            title: "test3",
-            description: "test3 desc"
-        });
-        placesArray.push({
-            latitude: 47,
-            longitude: 15,
-            id: "3",
-            title: "test2",
-            description: "test2 desc"
-        });
-        const usersMarkers = placesArray.map(userPlace => <MapView.Marker coordinate={userPlace} key={userPlace.id} />);
-
-        //REAL DATA
-        const placesArrayReal = [];
-        fetch("https://our-rent-api.herokuapp.com/api/objects")
-            .then(function (response) {
-                response.json().then(function (objects) {
-                    var index = 0;
+        const getData = () => new Promise(resolve => {
+            const placesArrayReal = [];
+            fetch('https://our-rent-api.herokuapp.com/api/objects')
+                .then(res => res.json())
+                .then(async (objects) => {
+                    let index = 0;
                     for (let item of objects) {
-                        fetch("https://maps.google.com/maps/api/geocode/json?key=AIzaSyCIGc4fL0PJv0smNrtUsHylALwAeoygHnI&address=" + item.address).then(function (coordinates) {
-                            coordinates.json().then(function (data) {
-                                var location = data.results[0].geometry.location;
-                                index = index + 1;
-                                placesArrayReal.push({
-                                    latitude: location.lat,
-                                    longitude: location.lng,
-                                    id: index.toString(),
-                                    title: "test" + index.toString(),
-                                    description: "test" + index.toString()
-                                });
-                            })
-
-                        })
+                        const res = await fetch(`https://maps.google.com/maps/api/geocode/json?key=AIzaSyCIGc4fL0PJv0smNrtUsHylALwAeoygHnI&address=${item.address}`);
+                        const data = await res.json();
+                        index++;
+                        const location = data.results[0].geometry.location;
+                        placesArrayReal.push({
+                            latitude: location.lat,
+                            longitude: location.lng,
+                            id: index + '',
+                            title: 'Title ' + index,
+                            description: 'Descr ' + index
+                        });
                     }
-                })
-            })
-        console.log(placesArray);
-        console.log(placesArrayReal);
-        navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({
-                    latitude: position.coords.latitude,
-                    longitude: position.coords.longitude,
-                    markers: usersMarkers,
-                    error: null,
+                    resolve(placesArrayReal);
                 });
-            },
-            (error) => this.setState({ error: error.message }),
-            { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
-        );
+        });
+
+        getData().then(data => {
+            const usersMarkers = data.map(userPlace => <MapView.Marker coordinate={userPlace} key={userPlace.id} />);
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    this.setState({
+                        latitude: data[0].latitude,
+                        longitude: data[0].longitude,
+                        markers: usersMarkers,
+                        error: null,
+                    });
+                },
+                (error) => this.setState({ error: error.message }),
+                { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+            );
+        });
     }
 
     render() {
