f6d40aa4f0be9f09c81223d703a540ced6e7f13a
Jaka Tertinek <tertinek.jaka@gmail.com>
2018-05-24 18:36:29 +0200

    bug in package

--- android/app/src/main/java/com/ourrent/MainApplication.java
+++ android/app/src/main/java/com/ourrent/MainApplication.java
@@ -1,7 +1,7 @@
 package com.ourrent;
 
 import android.app.Application;
-
+import org.reactnative.camera.RNCameraPackage;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -23,6 +23,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCameraPackage(),
           new MapsPackage()
       );
     }
